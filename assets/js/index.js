324f5d3ead47f669a06afde47ce880eac4dbcf04
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-06-20 13:20:24 +0100

    pushing new changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,45 @@ function handleNavLinks(event) {
   console.log(event.target);
   event.target.classList.add("active");
 }
-
 links.forEach((link) => {
   link.addEventListener("click", handleNavLinks);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const sections = document.querySelectorAll("section[id]");
+  const header = document.querySelector("header[id]");
+
+  // Function to remove the active class from all nav links
+  const removeActiveClasses = () => {
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+  };
+
+  // Function to add the active class to the current section nav link
+  const addActiveClass = () => {
+    let currentSection = "home";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - header.offsetHeight;
+
+      if (window.scrollY === 0) {
+        currentSection = header.getAttribute("id");
+      } else if (window.scrollY >= sectionTop - 60) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+
+    removeActiveClasses();
+    navLinks.forEach((link) => {
+      if (link.getAttribute("href").includes(currentSection)) {
+        link.classList.add("active");
+      }
+    });
+  };
+  addActiveClass();
+  window.addEventListener("scroll", addActiveClass);
 });
 
 function handleGetinTouch(getInTouchButton) {
